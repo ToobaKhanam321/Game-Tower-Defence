e14c09613079c0ce3bc4c272eef6a8aa9dac0c9a
UroobaSattar <82996273+UroobaSattar@users.noreply.github.com>
2021-07-20 15:36:04 +0500

    function

---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+
+
+import pygame
+from core.game import Game
+from core.window import Window
+
+# Init pygame
+pygame.init()
+
+# Create a window
+window = Window(1280, 768)
+window.set_title("Tower Defence")
+window.set_background(148, 168, 176)
+
+# Create the game instance
+game = Game(window)
+game.run()
+
+# Quit pygame
+pygame.quit()
